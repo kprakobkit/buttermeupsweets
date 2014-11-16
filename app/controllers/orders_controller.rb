390229c3cc76aaf06807138eef6ff16d72add269
Peter Prakobkit <kprakobkit@gmail.com>
2014-11-16 15:52:19 -0800

    implement create orders in controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,13 +3,29 @@ class OrdersController < ApplicationController
   end
 
   def new
-    @items = Item.all
     @order = Order.new
+    @items = Item.all
   end
 
   def create
-    @order = Order.new
-    @items = Item.all
-    render :new
+    @order = Order.new(order_params)
+    items = params[:item_id]
+    quantities = params[:quantity]
+    items_quantities = items.zip(quantities)
+    items_quantities.each do |item_quantity|
+      @order.item_orders.new(item_id: item_quantity[0], item_quantity: item_quantity[1])
+    end
+
+    if @order.save
+      render :index
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit!
   end
 end
