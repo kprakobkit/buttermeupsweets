3f54206def883988b0b94c87feeb92393f227881
Peter Prakobkit <kprakobkit@gmail.com>
2014-11-17 17:53:35 -0800

    add scroll down button

--- app/assets/javascripts/orders.js
+++ app/assets/javascripts/orders.js
@@ -2,4 +2,16 @@ $(document).ready(function() {
   $('#addItems').click(function() {
     $('.addItemsForm:last').clone().show().appendTo('.items');
   });
+  
+  var windowHeight = $(window).height();
+
+  $('.button-down').click(function(){
+    $('html,body').animate({ scrollTop: windowHeight}, 600);
+    return false;
+  });
+
+  $('.button-down2').click(function(){
+    $('html,body').animate({ scrollTop: 2*windowHeight}, 600);
+    return false;
+  });
 });
