d3ce30ab1901b93d91ac1d6b9dda5f859bd10584
Peter Prakobkit <kprakobkit@gmail.com>
2014-11-18 20:32:49 -0800

    fix bugs on rendering item information in email template

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -4,14 +4,14 @@ class OrderMailer < ActionMailer::Base
   def order_confirmation_email(order)
     email = order.customer_contact_info
     @customer_name = order.customer_name
-    @items = order.items
+    @items = order.item_orders
     @total = order.order_total
     mail(to: email, subject: "Order Confirmation from Butter Me Up Sweets")
   end
 
   def order_confirmation_email_natalie(order)
     @customer_name = order.customer_name
-    @items = order.items
+    @items = order.item_orders
     @total = order.order_total
     mail(to: "natalie@buttermeupsweets.com", subject: "Order made on Butter Me Up Sweets")
   end
